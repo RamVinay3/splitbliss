b0c92598c0268b76437a2f6a3c69ded8297e0446
Ramvinay3 <ramvinay609@gmail.com>
2023-09-07 15:43:44 +0530

    added splshscreen

---
+++ lib/screens/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            child: Text(''),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:splitbliss/widgets/svg.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  static const String iconpath = 'lib/svg/splashIcon.svg';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 29, 70, 216),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisSize: MainAxisSize.min,
+          children: [
+            const SVG(svgPath: 'lib/svg/splashIcon.svg'),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              'SplitBliss',
+              style: GoogleFonts.robotoSerif(
+                fontSize: 34,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 1
+                  ..color = Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
